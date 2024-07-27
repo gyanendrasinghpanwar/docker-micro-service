1563c05254823fc9cc40c38eb7994c0a9c7cc44b
Gyanendra <gyanendrapanwar371@gmail.com>
2024-07-27 14:07:28 +0530

    Added binary search api

--- go-api/main.go
+++ go-api/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type RequestBody struct {
+	Numbers []int `json:"numbers" binding:"required"`
+	Target  int   `json:"target" binding:"required"`
+}
+
 func main() {
 	r := gin.Default()
 	fmt.Println("Added router")
@@ -34,6 +39,40 @@ func main() {
 		})
 	})
 
-	// Run the server on port 8080
+	r.POST("/binary-search", func(c *gin.Context) {
+		var requestBody RequestBody
+
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		numbers := requestBody.Numbers
+		index := binarySearch(numbers, requestBody.Target)
+
+		c.JSON(200, gin.H{
+			"index": index,
+		})
+	})
+
+	// Run the server on port 8081
 	r.Run(":8081")
+}
+
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1 // Target not found
 }
